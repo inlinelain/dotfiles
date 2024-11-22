a6b55e3b313827030b8aec9d2be6e5aba86ade83
inlinelain <inlinelain@tutanota.com>
2024-11-23 02:01:00 +0300

    Added config.lua

---
+++ etc/skel/.config/nvim/lua/config/config.lua
@@ -0,0 +1,12 @@
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.termguicolors = true
+vim.wo.number = true
+vim.o.syntax = 'enable'
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.g.mapleader = ' '
